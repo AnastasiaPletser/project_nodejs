8bc7e104744da88229a8f0cc7b155cc5be6ade3e
Anastasia <nastusya89pl@gmail.com>
2024-12-08 12:43:32 +0200

    Greated checkAuth and checkAdmin

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
-const URI = 'mongodb+srv://nastusya89pl:FzjBkRTIzrL6mHfy@anastasia.x0dp8.mongodb.net/?retryWrites=true&w=majority&appName=Anastasia';
+const URI = 'mongodb+srv://nastusya89pl:FzjBkRTIzrL6mHfy@anastasia.x0dp8.mongodb.net/?retryWrites=true&w=majority&appName=Anastasia'
 
-mongoose.connect(URI).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((e) => {
-    console.error(e);
-});
-
-
+mongoose.connect(URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((e) => {
+        console.error('MongoDB connection error:', e);
+    });
