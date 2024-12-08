ab2aab0398cfbe1974fb76e3d4156ffc567e921c
Anastasia <nastusya89pl@gmail.com>
2024-12-08 14:26:40 +0200

    add swagger

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import swaggerUI from 'swagger-ui-express';
 import './config/db.js';
 
 //Routes
 import authRouter from './routes/authRoutes.js';
 import taskRouter from './routes/taskRoutes.js';
+import swaggerSpec from './config/swagger.js';
 
 const app = express();
 const port = 3000;
 
 //middleware
 app.use(bodyParser.json());
+
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.use('/api', authRouter);
 app.use('/api', taskRouter);
@@ -19,4 +23,5 @@ app.use('/api', taskRouter);
     console.log(
       `Server listening on port ${port} and starting at http://localhost:${port}`
     );
+    console.log('Swagger docs available at http://localhost:3000/api-docs');
   });
